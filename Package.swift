9f22cef674cbefd0009e92bfdcf5d794eb4677b9
Kilian Koeltzsch <me@kilian.io>
2017-07-23 21:51:08 +0200

    update basic structure

--- Package.swift
+++ Package.swift
@@ -13,7 +13,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "apodidae",
+            name: "ApodidaeLib",
             dependencies: []),
+        .target(
+            name: "apo",
+            dependencies: ["ApodidaeLib"]),
     ]
 )
