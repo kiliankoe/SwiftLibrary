8d42a6df63c5ec7a783f6d55f2bb845e23a76fad
Kilian Koeltzsch <me@kilian.io>
2017-07-23 23:19:26 +0200

    add CommandLine and Rainbow dependencies

--- Package.swift
+++ Package.swift
@@ -8,15 +8,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jatoben/CommandLine", from: "3.0.0-pre1"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "2.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ApodidaeCore",
-            dependencies: ["CommandLine"]),
+            dependencies: []),
         .target(
             name: "apo",
+            dependencies: ["ApodidaeCore", "CommandLine", "Rainbow"]),
             dependencies: ["ApodidaeCore"]),
     ]
 )
