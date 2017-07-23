3663d3bf528b07e2f57ce6486f589b65ca3ffb75
Kilian Koeltzsch <me@kilian.io>
2017-07-23 22:01:33 +0200

    rename lib to core

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ApodidaeLib",
-            dependencies: []),
+            name: "ApodidaeCore",
+            dependencies: ["CommandLine"]),
         .target(
             name: "apo",
-            dependencies: ["ApodidaeLib"]),
+            dependencies: ["ApodidaeCore"]),
     ]
 )
