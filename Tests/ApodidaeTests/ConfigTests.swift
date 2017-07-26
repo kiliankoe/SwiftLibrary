1749c42e0dcb638c424f78a792424ccf906c4275
Kilian Koeltzsch <me@kilian.io>
2017-07-26 17:01:37 +0200

    add tests for weird key handling in config

--- Tests/ApodidaeTests/ConfigTests.swift
+++ Tests/ApodidaeTests/ConfigTests.swift
@@ -11,12 +11,60 @@ class ConfigTests: XCTestCase {
         """.data(using: .utf8)!
 
         let config = try! JSONDecoder().decode(Config.self, from: json)
-        XCTAssertEqual(config.librariesIOApiKey, "foobar")
+        XCTAssertEqual(config.lioAPIKey, "foobar")
         XCTAssertEqual(config.disableLibrariesIO, false)
 
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         let encoded = try! encoder.encode(config)
         XCTAssertEqual(encoded, json)
+    }
+
+    func testKeyEnabled() {
+        let json = """
+        {
+          "librariesio_api_key" : "foobar",
+          "disable_librariesio" : false
+        }
+        """.data(using: .utf8)!
+
+        let config = try! JSONDecoder().decode(Config.self, from: json)
+        XCTAssertEqual(config.lioAPIKey, "foobar")
+    }
+
+    func testKeyButDisabled() {
+        let json = """
+        {
+          "librariesio_api_key" : "foobar",
+          "disable_librariesio" : true
+        }
+        """.data(using: .utf8)!
+
+        let config = try! JSONDecoder().decode(Config.self, from: json)
+        XCTAssertNil(config.lioAPIKey)
+    }
+
+    func testEmptyKeyButEnabled() {
+        let json = """
+        {
+          "librariesio_api_key" : "",
+          "disable_librariesio" : false
+        }
+        """.data(using: .utf8)!
+
+        let config = try! JSONDecoder().decode(Config.self, from: json)
+        XCTAssertNil(config.lioAPIKey)
+    }
+
+    func testEmptyKeyDisabled() {
+        let json = """
+        {
+          "librariesio_api_key" : "",
+          "disable_librariesio" : true
+        }
+        """.data(using: .utf8)!
+
+        let config = try! JSONDecoder().decode(Config.self, from: json)
+        XCTAssertNil(config.lioAPIKey)
     }
 }
