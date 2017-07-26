f6901e81eb6b3e870983038938125aac1520b01b
Kilian Koeltzsch <me@kilian.io>
2017-07-26 21:41:51 +0200

    move package to its own file

--- Sources/ApodidaeCore/IBMPackageCatalog.swift
+++ Sources/ApodidaeCore/IBMPackageCatalog.swift
@@ -1,7 +1,5 @@
 import Foundation
 import PromiseKit
-import Rainbow
-import Releases
 
 struct PCResponse: Decodable {
     let packages: [Package]
@@ -23,62 +21,6 @@ struct PCResponse: Decodable {
         let dataContainer = try rootContainer.nestedContainer(keyedBy: DataKeys.self, forKey: .data)
         let hitsContainer = try dataContainer.nestedContainer(keyedBy: HitsKeys.self, forKey: .hits)
         self.packages = try hitsContainer.decode([Package].self, forKey: .hits)
-    }
-}
-
-public struct Package: Decodable {
-    public let name: String
-    public let description: String
-    public let gitCloneURL: URL
-    public let latestVersion: String?
-    public let stars: Int
-
-    public var repository: URL {
-        let urlString = self.gitCloneURL.absoluteString.replacingOccurrences(of: ".git", with: "")
-        return URL(string: urlString)!
-    }
-
-    public var versions: [Version] {
-        do {
-            return try Releases.versions(for: self.gitCloneURL)
-        } catch {
-            print("Encountered error on fetching releases: \(error)")
-            return []
-        }
-    }
-
-    public var cliRepresentation: String {
-        var output = """
-            - \(self.name.bold) \(self.latestVersion ?? "")
-              \((self.repository.absoluteString).italic)
-            """
-        if !self.description.isEmpty {
-            output += "\n  \(self.description)"
-        }
-        return output
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case name = "package_full_name"
-        case description
-        case gitCloneURL = "git_clone_url"
-        case latestVersion = "latest_version"
-        case stars = "stargazers_count"
-    }
-
-    private enum SourceKeys: String, CodingKey {
-        case source = "_source"
-    }
-
-    public init(from decoder: Decoder) throws {
-        let sourceContainer = try decoder.container(keyedBy: SourceKeys.self)
-        let container = try sourceContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .source)
-
-        self.name = try container.decode(String.self, forKey: .name)
-        self.description = try container.decode(String.self, forKey: .description)
-        self.gitCloneURL = try container.decode(URL.self, forKey: .gitCloneURL)
-        self.latestVersion = try container.decodeIfPresent(String.self, forKey: .latestVersion)
-        self.stars = try container.decode(Int.self, forKey: .stars)
     }
 }
 
