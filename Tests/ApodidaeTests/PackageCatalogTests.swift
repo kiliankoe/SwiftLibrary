e52d45c1a1738df42e40b57b4eac31440018d8e2
Kilian Koeltzsch <me@kilian.io>
2017-07-29 23:11:55 +0200

    comment out packagecatalog network test

--- Tests/ApodidaeTests/PackageCatalogTests.swift
+++ Tests/ApodidaeTests/PackageCatalogTests.swift
@@ -2,21 +2,21 @@ import XCTest
 @testable import ApodidaeCore
 
 class PackageCatalogTests: XCTestCase {
-    func testSearch() {
-        let e = expectation(description: "Get some data")
-
-        PackageCatalog.search(query: "apodidae", isVerbose: true).then { packages in
-            XCTAssert(packages.count > 0)
-            e.fulfill()
-        }.catch { error in
-            XCTFail("Failed with error: \(error)")
-            e.fulfill()
-        }
-
-        waitForExpectations(timeout: 5)
-    }
+//    func testSearch() {
+//        let e = expectation(description: "Get some data")
+//
+//        PackageCatalog.search(query: "apodidae", isVerbose: true).then { packages in
+//            XCTAssert(packages.count > 0)
+//            e.fulfill()
+//        }.catch { error in
+//            XCTFail("Failed with error: \(error)")
+//            e.fulfill()
+//        }
+//
+//        waitForExpectations(timeout: 5)
+//    }
 
     static var allTests = [
-        ("testSearch", testSearch),
+//        ("testSearch", testSearch),
     ]
 }
