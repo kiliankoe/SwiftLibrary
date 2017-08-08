39f83bcc28fda6690125b85b40f797e2d3cd3f43
Kilian Koeltzsch <me@kilian.io>
2017-08-08 15:30:11 +0200

    Add Git.ls(remote:)

--- Sources/ApodidaeCore/Git.swift
+++ Sources/ApodidaeCore/Git.swift
@@ -5,6 +5,7 @@ import ShellOut
 public enum Git {
     public enum Error: Swift.Error {
         case notARepo
+        case remoteNotFound
     }
 
     public static func uncommitedChanges(in dir: Folder = Folder.current) throws -> Bool {
@@ -12,12 +13,33 @@ public enum Git {
         guard !result.contains("Not a git repository") else { throw Error.notARepo }
         return !result.isEmpty
     }
+
+    public static func ls(remote: String) throws -> (heads: [String], tags: [String]) {
+        let result = try shellOut(to: "git", arguments: ["ls-remote", "--heads", "--tags", remote])
+        guard !result.contains("remote: Not Found") else { throw Error.remoteNotFound }
+        let refs = result
+            .split(separator: "\n")
+            .map { $0.split(separator: " ") }
+            .flatMap { $0.last }
+            .map(String.init)
+
+        let heads = refs
+            .filter { $0.hasPrefix("refs/heads") }
+            .map { $0.replacingOccurrences(of: "refs/heads/", with: "") }
+
+        let tags = refs
+            .filter { $0.hasPrefix("refs/tags") }
+            .map { $0.replacingOccurrences(of: "refs/tags/", with: "") }
+
+        return (heads, tags)
+    }
 }
 
 extension Git.Error: LocalizedError {
     public var errorDescription: String? {
         switch self {
         case .notARepo: return "Not a git repository (or any of the parent directories)"
+        case .remoteNotFound: return "Remote not found"
         }
     }
 }
