e7cd2728373f84e0912f73629eed50e03514d1dd
Kilian Koeltzsch <me@kilian.io>
2017-11-17 12:06:53 +0100

    remove CLISpinner from ApodidaeCore target

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ApodidaeCore",
-            dependencies: ["PromiseKit", "Files", "Rainbow", "Regex", "CLISpinner", "ShellOut"]),
+            dependencies: ["PromiseKit", "Files", "Rainbow", "Regex", "ShellOut"]),
         .target(
             name: "swift-catalog",
             dependencies: ["ApodidaeCore", "CommandLine", "CLISpinner", "Signals"]),
