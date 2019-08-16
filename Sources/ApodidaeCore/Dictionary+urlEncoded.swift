a5aaf475c758bcb61731289a1fcf975f9926184a
Kilian Koeltzsch <me@kilian.io>
2019-08-17 01:42:55 +0200

    delete old apodidae content

--- Sources/ApodidaeCore/Dictionary+urlEncoded.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension Dictionary where Key == String {
-    var asJSON: Data? {
-        return try? JSONSerialization.data(withJSONObject: self)
-    }
-}
--- Sources/ApodidaeCore/String+urlEscaped.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension String {
-    var urlHostEscaped: String? {
-        return self.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
-    }
-}
