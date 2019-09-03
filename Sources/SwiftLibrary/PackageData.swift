a21af6b730768e1032068c98bcabc35488f5662d
Kilian Koeltzsch <me@kilian.io>
2019-09-03 14:53:20 +0200

    Support new version specific metadata

--- Sources/SwiftLibrary/PackageData.swift
+++ Sources/SwiftLibrary/PackageData.swift
@@ -7,26 +7,40 @@ public struct PackageData: Decodable {
     public let repositoryIdentifier: String
     public let name: String
     public let description: String?
-    public let versions: Versions?
-    public let releasedAgo: String?
-    public let swiftVersions: String
     public let license: String?
-    public let supportedMacosVersion: String?
-    public let supportedIosVersion: String?
-    public let supportedWatchosVersion: String?
-    public let supportedTvosVersion: String?
-    public let libraryCount: Int
-    public let executableCount: Int
-    public let stargazers: Int
-    public let watchers: Int
-    public let searchScore: Int
+    public let stars: Int
     public let updatedAt: String // FIXME
+
+    public let versions: [Version]
+
+    public var latest: Version {
+        guard let first = self.versions.first else {
+            fatalError("No version found, according to Dave this shouldn't happen :P")
+        }
+        return first
+    }
+
+    public var latestRelease: Version? {
+        self.versions.first { $0.kind == .tag }
+    }
 }
 
 extension PackageData {
-    public struct Versions: Decodable {
-        public let latest: String
-        public let latest_stable: String?
+    public struct Version: Decodable {
+        public let name: String
+        public let kind: Kind
+        public let releasedAgo: String?
+        public let swiftVersions: String
+        public let supportedMacosVersion: String?
+        public let supportedIosVersion: String?
+        public let supportedWatchosVersion: String?
+        public let supportedTvosVersion: String?
+        public let libraryCount: Int
+        public let executableCount: Int
+
+        public enum Kind: String, Decodable {
+            case branch, tag
+        }
     }
 }
 
@@ -43,31 +57,33 @@ extension PackageData {
     }
 
     public var longDescription: String {
+        let libraryString = latest.libraryCount == 1 ? "library" : "libraries"
+        let executableString = latest.executableCount == 1 ? "executable" : "executables"
+
         var output = """
-        \(repositoryIdentifier) \(versions?.latest ?? "") \(versions?.latest_stable ?? "")
+        \(repositoryIdentifier) \(latest.name) \(latestRelease?.name ?? "")
         \(description ?? "No description available")
 
-        \(stargazers) stargazers
-        \(watchers) watchers
+        \(stars) stargazers
 
         Licensed under \(license ?? "n/a").
-        Supports Swift \(swiftVersions).
-        Last released \(releasedAgo ?? "n/a") ago.
-        Contains \(libraryCount) library/libraries.
-        Contains \(executableCount) executable(s).
+        Supports Swift \(latest.swiftVersions).
+        Last released \(latestRelease?.releasedAgo ?? "n/a") ago.
+        Contains \(latest.libraryCount) \(libraryString).
+        Contains \(latest.executableCount) \(executableString).
         
         """
 
-        if let macOS = supportedMacosVersion {
+        if let macOS = latest.supportedMacosVersion {
             output += "\nSupports macOS \(macOS)"
         }
-        if let iOS = supportedIosVersion {
+        if let iOS = latest.supportedIosVersion {
             output += "\nSupports iOS \(iOS)"
         }
-        if let tvOS = supportedTvosVersion {
+        if let tvOS = latest.supportedTvosVersion {
             output += "\nSupports tvOS \(tvOS)"
         }
-        if let watchOS = supportedWatchosVersion {
+        if let watchOS = latest.supportedWatchosVersion {
             output += "\nSupports watchOS \(watchOS)"
         }
         return output
